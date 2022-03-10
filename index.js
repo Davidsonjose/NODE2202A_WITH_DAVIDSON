7ecf7d2b1380bdcf2a22c27b19b1a13ed0c37243
Davidson jose <91620722+Davidsonjose@users.noreply.github.com>
2022-03-10 04:05:31 +0100

    syncronous and asyncronous method of performing tasks

--- index.js
+++ index.js
@@ -1,23 +1,111 @@
-var name = "lanre";
-var age = 50;
-var complexion = "fair";
-var person = `my name is ${name}, i am ${age} years old and i am ${complexion} in complexion`;
-person;
-function calYearOfBirth(dateOfBirth) {
-  let currentYear = new Date().getFullYear();
-  let calYear = currentYear - dateOfBirth;
-  return calYear;
-}
+//today class talking on file system and path
+
+//fs File system
+const fs = require("fs");
+const path = require("path");
+
+//SYNCRONOUS METHOD OF READING AND WRITING TO A FILE
+let pathFolderOne = path.join(__dirname, "files/fileOne.txt");
+
+const findContent = fs.readFileSync(pathFolderOne, "utf-8");
+console.log(findContent);
+
+console.log("Syncronous method is just there jire");
+
+let writeToPathFolderOne = fs.writeFileSync(pathFolderOne, 'Sorry you have just been overwritten');
+
+console.log("You have successfully written to" + pathFolderOne);
+
+console.log(pathFolderOne);
+
+// ASYNCRONOUS METHOD OF READING, WRITING, CREATING A FOLDER, RENAMING A FOLDER, DELETING A FOLDER, CREATING A FILE, DELETING A FILE
+
+//READING A FILE
+fs.readFile(pathFolderOne, (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(
+      "You just read to a file from " +
+        pathFolderOne +
+        " " +
+        "here is the the result we got: " +
+        data
+    );
+  }
+});
+
+console.log("I love all the asyncronous methods .........");
+
+const pathFolderTwo = path.join(__dirname, "files/filetwo.txt ");
+
+//CREATING A FILE
+fs.appendFile(pathFolderTwo, "I am the second file just created", (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(
+      "A new file " +
+        pathFolderTwo +
+        " " +
+        "has been created successfully " +
+        "content: "
+    );
+  }
+});
+
+const newFolder = path.join(__dirname, "file2/");
+//creating a folder
+fs.mkdir(newFolder, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("a new folder has been created");
+    //CREATING A FILE
+    fs.appendFile(
+      newFolder + "/one.txt",
+      "I am the second file just created",
+      (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(
+            "A new file " +
+              newFolder +
+              " " +
+              "has been created successfully " +
+              "content: "
+          );
+        }
+      }
+    );
+
+    fs.readFile(newFolder + "/one.txt", (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Here is the content you requested: " + data);
+      }
+    });
+  }
+});
+
+const removeFolder = path.join(__dirname, "one");
+fs.rmdir(removeFolder, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Folder: " + removeFolder + " has been removed successfully");
+  }
+});
 
 
-const data= [
-    {name: "davidson", age: 30, complexion: "Dark"},
-    {name: "Lanre", age: 100, complexion: "Purple"},
-    {name: "Bisi", age: 50, complexion: "Fair"},
-]
-
-module.exports = {
-  calYearOfBirth,
-  data
-};
-// console.log(person);
+const renameFilePath = path.join(__dirname, 'file2/one.txt');
+const renameFilePathNew = path.join(__dirname, 'file2/five.txt')
+fs.rename(renameFilePath, renameFilePathNew, (err)=>{
+  if (err) {
+    console.log(err);
+  }else{
+    console.log("File: " + renameFilePath + " " + "has been renamed to " + renameFilePathNew)
+  }
+})
